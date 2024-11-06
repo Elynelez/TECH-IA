f8c2d0e2b7e7950e4b241733fa817cc1d4de164c
Elynelez <johndoe@example.comElinarnyex@gmail.com>
2024-11-06 15:02:48 -0500

    nuevo formato

--- helpers/cutAudios.py
+++ helpers/cutAudios.py
@@ -1,28 +1,28 @@
 import os
 from pydub import AudioSegment
-import gdown
 
 def cut_and_normalize_audios():
-    segment_length = 5000  # Duración de los segmentos en milisegundos (5 segundos)
-    dataset_path = 'dataset'
+    
+    # segment_length = 5000  # Duración de los segmentos en milisegundos (5 segundos)
+    # dataset_path = 'dataset'
 
-    for file_name in os.listdir(dataset_path):
-        org_path = os.path.join(dataset_path, file_name, 'org')
-        cut_path = os.path.join(dataset_path, file_name, 'cut')
+    # for file_name in os.listdir(dataset_path):
+    #     org_path = os.path.join(dataset_path, file_name, 'org')
+    #     cut_path = os.path.join(dataset_path, file_name, 'cut')
 
-        if os.path.exists(cut_path):
-            continue
+    #     if os.path.exists(cut_path):
+    #         continue
         
-        # Crea la carpeta 'cut' si no existe
-        os.makedirs(cut_path, exist_ok=True)
+    #     # Crea la carpeta 'cut' si no existe
+    #     os.makedirs(cut_path, exist_ok=True)
         
-        # Procesa cada archivo en la carpeta 'org'
-        for audio_file in os.listdir(org_path):
-            if audio_file.endswith('.wav'):
-                audio = AudioSegment.from_wav(os.path.join(org_path, audio_file))
+    #     # Procesa cada archivo en la carpeta 'org'
+    #     for audio_file in os.listdir(org_path):
+    #         if audio_file.endswith('.wav'):
+    #             audio = AudioSegment.from_wav(os.path.join(org_path, audio_file))
                 
-                # Segmenta el audio en fragmentos de 5 segundos
-                for i, start in enumerate(range(0, len(audio), segment_length)):
-                    segment = audio[start:start + segment_length]
-                    segment = segment.apply_gain(-segment.max_dBFS)  # Normalización
-                    segment.export(os.path.join(cut_path, f"{audio_file}_part{i}.wav"), format="wav")
+    #             # Segmenta el audio en fragmentos de 5 segundos
+    #             for i, start in enumerate(range(0, len(audio), segment_length)):
+    #                 segment = audio[start:start + segment_length]
+    #                 segment = segment.apply_gain(-segment.max_dBFS)  # Normalización
+    #                 segment.export(os.path.join(cut_path, f"{audio_file}_part{i}.wav"), format="wav")
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const { exec } = require('child_process'); // Para ejecutar scripts de Python
 const path = require('path'); // Para manejar rutas de archivos
 const app = express();
@@ -21,6 +22,39 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 const databasePath = path.join(__dirname, 'public/database.json');
 const datasetPath = path.join(__dirname, 'dataset');
+
+function readDatabase() {
+    const data = fs.readFileSync(databasePath, 'utf8');
+    return JSON.parse(data).dataset;
+}
+
+async function downloadAudioFiles() {
+    const datasetPath = path.join(__dirname, 'dataset');
+    const driveFiles = readDatabase(); // Lee el archivo JSON
+
+    for (const [folderName, folderContent] of Object.entries(driveFiles)) {
+        const orgPath = path.join(datasetPath, folderName, 'org');
+        fs.mkdirSync(orgPath, { recursive: true });
+
+        for (const [fileName, fileId] of Object.entries(folderContent.org)) {
+            const localFilePath = path.join(orgPath, fileName);
+
+            // Solo descargar si el archivo no existe
+            if (!fs.existsSync(localFilePath)) {
+                const url = `https://drive.google.com/uc?export=download&id=${fileId}`;
+                const response = await fetch(url);
+
+                if (response.ok) {
+                    const fileStream = fs.createWriteStream(localFilePath);
+                    response.body.pipe(fileStream);
+                    console.log(`Descargado: ${fileName} en ${orgPath}`);
+                } else {
+                    console.error(`Error al descargar ${fileName}: ${response.statusText}`);
+                }
+            }
+        }
+    }
+}
 
 function getAudioCounts() {
     const result = {};
@@ -55,64 +89,69 @@ function getAudioCounts() {
     return result;
 }
 
-app.get('/api/audios', (req, res) => {
-    const audioCounts = getAudioCounts();
-    res.json(audioCounts);
-});
+(async () => {
+    await downloadAudioFiles();
 
-app.post('/api/python', upload.single('audio'), (req, res) => {
-    console.log(req.body)
-
-    if (!req.file) {
-        return res.json({ message: "No se ha cargado ningún archivo." });
-    }
-
-    const { username, password, slang } = req.body;
-
-    // Leer el archivo database.json
-    fs.readFile(databasePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error al leer el archivo de base de datos:', err);
-            return res.json({ message: "error al leer la base de datos" })
+    await app.get('/api/audios', (req, res) => {
+        const audioCounts = getAudioCounts();
+        res.json(audioCounts);
+    });
+    
+    await app.post('/api/python', upload.single('audio'), (req, res) => {
+        console.log(req.body)
+    
+        if (!req.file) {
+            return res.json({ message: "No se ha cargado ningún archivo." });
         }
-
-        // Parsear el contenido JSON
-        const usersData = JSON.parse(data);
-        const user = usersData.users.find(user => user.user === username && user.password === password);
-
-        // Comprobar si el usuario existe y la contraseña es correcta
-        if (!user) {
-            return res.json({ message: 'Usuario o contraseña incorrectos.' });
-        }
-
-        const audioFilePath = req.file.path; // Ruta del archivo de audio
-        const pythonScriptPath = path.join(__dirname, 'index.py');
-        const pythonCommand = process.platform === "win32" ? "python" : "python3";
-
-        // Ejecutar el script de Python
-        exec(`${pythonCommand} "${pythonScriptPath}" "${audioFilePath}"`, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error ejecutando el script: ${error.message}`);
-                return res.json({ message: 'Error ejecutando el script de Python.' });
+    
+        const { username, password, slang } = req.body;
+    
+        // Leer el archivo database.json
+        fs.readFile(databasePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error al leer el archivo de base de datos:', err);
+                return res.json({ message: "error al leer la base de datos" })
             }
-            if (stderr) {
-                console.error(`Error en el script: ${stderr}`);
-                return res.json({ message: 'Error en el script de Python.' });
+    
+            // Parsear el contenido JSON
+            const usersData = JSON.parse(data);
+            const user = usersData.users.find(user => user.user === username && user.password === password);
+    
+            // Comprobar si el usuario existe y la contraseña es correcta
+            if (!user) {
+                return res.json({ message: 'Usuario o contraseña incorrectos.' });
             }
-
-            // Procesar la salida del script Python
-            const response = JSON.parse(stdout);
-            if (response.message.toLowerCase() == slang.toLowerCase()) {
-                res.json(response)
-            } else {
-                res.json({ message: 'La frase no coincide.' })
-            }
-
+    
+            const audioFilePath = req.file.path; // Ruta del archivo de audio
+            const pythonScriptPath = path.join(__dirname, 'index.py');
+            const pythonCommand = process.platform === "win32" ? "python" : "python3";
+    
+            // Ejecutar el script de Python
+            exec(`${pythonCommand} "${pythonScriptPath}" "${audioFilePath}"`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error ejecutando el script: ${error.message}`);
+                    return res.json({ message: 'Error ejecutando el script de Python.' });
+                }
+                if (stderr) {
+                    console.error(`Error en el script: ${stderr}`);
+                    return res.json({ message: 'Error en el script de Python.' });
+                }
+    
+                // Procesar la salida del script Python
+                const response = JSON.parse(stdout);
+                if (response.message.toLowerCase() == slang.toLowerCase()) {
+                    res.json(response)
+                } else {
+                    res.json({ message: 'La frase no coincide.' })
+                }
+    
+            });
         });
     });
-});
+    
+    // Iniciar el servidor
+    await app.listen(port, () => {
+        console.log(`Servidor escuchando en http://localhost:${port}`);
+    });
+})();
 
-// Iniciar el servidor
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
-});
